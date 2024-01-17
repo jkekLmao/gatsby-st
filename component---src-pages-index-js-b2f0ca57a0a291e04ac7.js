a59fd0cf2c23f54d618bbca6f2a1e1bdc5c4c89e
jkekLmao <jkek951@gmail.com>
2024-01-18 00:09:55 +0200

    Updates

---
+++ component---src-pages-index-js-b2f0ca57a0a291e04ac7.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_st=self.webpackChunkgatsby_st||[]).push([[678],{6558:function(e,t,a){a.r(t),a.d(t,{Head:function(){return l}});var n=a(7294);const s={color:"#232129",padding:96,fontFamily:"-apple-system, Roboto, sans-serif, serif"},o={marginTop:0,marginBottom:64,maxWidth:320};t.default=()=>n.createElement("main",{style:s},n.createElement("h1",{style:o},"looool"));const l=()=>n.createElement("title",null,"Home Page")}}]);
+//# sourceMappingURL=component---src-pages-index-js-b2f0ca57a0a291e04ac7.js.map
